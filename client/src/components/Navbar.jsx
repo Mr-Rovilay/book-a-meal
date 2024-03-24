861cc68c565e350320ed0ce976368c10f534b558
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-03-24 09:29:12 +0100

    balling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -136,7 +136,7 @@ const Navbar = ({ text, icon }) => {
                 <div className="relative flex items-center justify-center">
                   <BsCart className="fill w-8 h-8 md:w-5 md:h-5" />
                   <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-red flex items-center justify-center">
-                    <p className="text-white font-semibold">0</p>
+                    <p className="text-white font-semibold">1</p>
                   </div>
                 </div>
               </Link>
