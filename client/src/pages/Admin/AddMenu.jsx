03189e6a0f8bb94b2f2009b3e9c096453d952f5d
Mr-Rovilay <ayodejiakinola007@gmail.com>
2024-03-29 02:56:27 +0100

    improvedd login

--- client/src/pages/Admin/AddMenu.jsx
+++ client/src/pages/Admin/AddMenu.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { FaUtensils } from "react-icons/fa";
 
 const AddMenu = () => {
+  let characterLimit = 100;
+  const [length, setLength] = useState(0);
+
   return (
     <div className="w-full md:w-[870px] px-4 mx-auto">
       <h2 className="text-2xl font-semibold my-4">
@@ -17,7 +21,7 @@ const AddMenu = () => {
             <input
               type="text"
               placeholder="Recipe Name"
-              className="input input-bordered w-full"
+              className="input-box pl-4"
             />
           </div>
 
@@ -28,7 +32,10 @@ const AddMenu = () => {
               <label className="label">
                 <span className="label-text">Category*</span>
               </label>
-              <select className="select select-bordered" defaultValue="default">
+              <select
+                className="select select-bordered "
+                defaultValue="default"
+              >
                 <option disabled value="default">
                   Select a category
                 </option>
@@ -60,9 +67,14 @@ const AddMenu = () => {
               <span className="label-text">Recipe Details</span>
             </label>
             <textarea
-              className="textarea textarea-bordered h-24"
+              className="h-40 resize-none leading-7 input-box"
               placeholder="Tell the worlds about your recipe"
+              onChange={(e) => setLength(e.target.value.length)}
+              maxLength={characterLimit}
             ></textarea>
+            <p className="mt-1 text-dark-grey text-sm text-right">
+              {characterLimit - length} characters left
+            </p>
           </div>
 
           {/* 4th row */}
--- client/src/pages/Admin/UpdateMenu.jsx
+++ client/src/pages/Admin/UpdateMenu.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { FaUtensils } from "react-icons/fa";
 
 const UpdateMenu = () => {
+  let characterLimit = 100;
+  const [length, setLength] = useState(0);
   return (
     <div className="w-full md:w-[870px] px-4 mx-auto">
       <h2 className="text-2xl font-semibold my-4">
@@ -17,7 +20,7 @@ const UpdateMenu = () => {
             <input
               type="text"
               placeholder="Recipe Name"
-              className="input input-bordered w-full "
+              className="input-box pl-4 "
             />
           </div>
 
@@ -52,7 +55,7 @@ const UpdateMenu = () => {
               <input
                 type="number"
                 placeholder="Price"
-                className="input input-bordered w-full"
+                className="input-box pl-4"
               />
             </div>
           </div>
@@ -63,9 +66,14 @@ const UpdateMenu = () => {
               <span className="label-text">Recipe Details</span>
             </label>
             <textarea
-              className="textarea textarea-bordered h-24"
+              className="h-40 resize-none leading-7 input-box"
               placeholder="Tell the worlds about your recipe"
+              onChange={(e) => setLength(e.target.value.length)}
+              maxLength={characterLimit}
             ></textarea>
+            <p className="mt-1 text-dark-grey text-sm text-right">
+              {characterLimit - length} characters left
+            </p>
           </div>
 
           {/* 4th row */}
